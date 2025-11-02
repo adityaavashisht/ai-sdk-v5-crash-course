c01652a7e915b75400084110665e93830da2be64
Aditya Vashisht <adityavashisht03@gmail.com>
2025-11-02 14:50:34 -0500

    generated text for the first on terminal

--- exercises/01-ai-sdk-basics/01.04-generating-text/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.04-generating-text/problem/main.ts
@@ -3,10 +3,10 @@ import { generateText } from 'ai';
 
 // TODO: Choose a model. I recommend using the Google Gemini model:
 // gemini-2.0-flash-lite
-const model = TODO;
+const model = google("gemini-2.0-flash-lite");
 
-const prompt = 'What is the capital of France?';
+const prompt = 'Which countries should you explore first if you are travelling to Europe for the first time?';
 
-const result = TODO; // TODO: Use generateText to get the result
+const result = await generateText({model, prompt})
 
 console.log(result.text);
